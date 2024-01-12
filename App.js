766ddd73a94d8f81b0460e39fcff899d0efe3cd1
Raul Zarza <raulzarza.dev@gmail.com>
2024-01-11 21:27:42 -0700

    small change

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Bienvenido a BajaRent</Text>
+      <Text>Bienvenido a BajaRent 2</Text>
       <StatusBar style="auto" />
     </View>
   )
