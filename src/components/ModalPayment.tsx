6f0851892e60af5c8a282f3dfe74f1d72ba97c46
Raul Zarza <raulzarza.dev@gmail.com>
2024-02-11 14:41:01 -0700

    update start with payments

---
+++ src/components/ModalPayment.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ModalPayment = () => {
+  return (
+    <View>
+      <Text>ModalPayment</Text>
+    </View>
+  )
+}
+
+export default ModalPayment
+
+const styles = StyleSheet.create({})
