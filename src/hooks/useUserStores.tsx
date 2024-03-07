ea8523304b167687edeba8eb11f6a2c2905601b1
Raul Zarza <raulzarza.dev@gmail.com>
2024-03-06 23:26:45 -0600

    update get store to format from serivces in profile

--- src/hooks/useUserStores.tsx
+++ src/hooks/useUserStores.tsx
@@ -35,7 +35,13 @@ function useUserStores() {
       .then(async (positions) => {
         const positionsWithStoreDataPromises = positions.map(
           async (position) => {
-            const store = userStores?.find((s) => s?.id === position?.storeId)
+            const store = await ServiceStores?.get(position?.storeId).catch(
+              (e) => {
+                console.error(e)
+                return null
+              }
+            )
+            // const store = userStores?.find((s) => s?.id === position?.storeId)
             return {
               ...position,
               store: {
@@ -52,6 +58,8 @@ function useUserStores() {
       })
       .catch(console.error)
   }
+
+  console.log({ userPositions })
 
   return {
     stores: [...userStores, ...userPositions],
