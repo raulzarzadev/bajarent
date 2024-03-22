cd3c88d3e5e70c6bb337c10d29717b37f20aabdd
Raul Zarza <raulzarza.dev@gmail.com>
2024-03-22 10:39:22 -0700

    hotfix(orderStatus) dont make repair order type with expiresAt

--- src/libs/orderStatus.ts
+++ src/libs/orderStatus.ts
@@ -1,11 +1,15 @@
 import { isBefore } from 'date-fns'
-import OrderType, { OrderStatus, order_status } from '../types/OrderType'
+import OrderType, {
+  OrderStatus,
+  order_status,
+  order_type
+} from '../types/OrderType'
 import asDate from './utils-date'
 import expireDate from './expireDate'
 
 const orderStatus = (order: Partial<OrderType>): OrderStatus => {
   if (order.status === order_status.PICKUP) return order.status
-
+  if (order.type === order_type.REPAIR) return order.status
   let status: OrderStatus = order?.status || order_status.PENDING
 
   const expireAt = expireDate(
