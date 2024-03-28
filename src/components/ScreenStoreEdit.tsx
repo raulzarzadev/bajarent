5a5eb9075d47c51357b0de260d49e33550446f46
Raul Zarza <raulzarza.dev@gmail.com>
2024-03-28 11:34:11 -0700

    fix (screenEditStore) remove scroll

--- src/components/ScreenStoreEdit.tsx
+++ src/components/ScreenStoreEdit.tsx
@@ -10,36 +10,38 @@ const ScreenStoreEdit = ({ navigation }) => {
   const { store, updateUserStores } = useStore()
   if (!store) return <ActivityIndicator />
   return (
-    <ScrollView style={gStyles.container}>
-      <FormStore
-        defaultValues={store}
-        onSubmit={async (values) => {
-          ServiceStores.update(store.id, values)
-            .then((res) => {
-              console.log(res)
-              navigation.goBack()
-            })
-            .catch(console.error)
-        }}
-      />
-      <View style={{ marginTop: 36 }}>
-        <ButtonConfirm
-          icon="delete"
-          text="Eliminar tienda de forma permanente"
-          openColor="error"
-          openLabel="Eliminar"
-          confirmColor="error"
-          confirmLabel="Eliminar"
-          handleConfirm={async () => {
-            ServiceStores.delete(store.id)
-              .then(console.log)
-              .catch(console.error)
-              .finally(() => {
-                updateUserStores()
+    <ScrollView>
+      <View style={gStyles.container}>
+        <FormStore
+          defaultValues={store}
+          onSubmit={async (values) => {
+            ServiceStores.update(store.id, values)
+              .then((res) => {
+                console.log(res)
+                navigation.goBack()
               })
-            navigation.goBack()
+              .catch(console.error)
           }}
         />
+        <View style={{ marginTop: 36 }}>
+          <ButtonConfirm
+            icon="delete"
+            text="Eliminar tienda de forma permanente"
+            openColor="error"
+            openLabel="Eliminar"
+            confirmColor="error"
+            confirmLabel="Eliminar"
+            handleConfirm={async () => {
+              ServiceStores.delete(store.id)
+                .then(console.log)
+                .catch(console.error)
+                .finally(() => {
+                  updateUserStores()
+                })
+              navigation.goBack()
+            }}
+          />
+        </View>
       </View>
     </ScrollView>
   )
