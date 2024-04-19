318b01b0cbb27abda0ed17246ba552594ef571c8
Raul Zarza <raulzarza.dev@gmail.com>
2024-04-19 14:31:56 -0700

    update (screenCashbox) add permissions to view balances

--- src/components/ScreenCashbox.tsx
+++ src/components/ScreenCashbox.tsx
@@ -2,26 +2,51 @@ import { View } from 'react-native'
 import React from 'react'
 import Button from './Button'
 import { gSpace, gStyles } from '../styles'
+import { useEmployee } from '../contexts/employeeContext'
 
 const ScreenCashbox = ({ navigation }) => {
+  const {
+    permissions: { isAdmin, isOwner, store }
+  } = useEmployee()
+  const canViewBalances = isAdmin || isOwner || store.canViewBalances
+  const canCreateBalance = isAdmin || isOwner || store.canCreateBalance
   return (
     <View style={[{ maxWidth: 500, marginHorizontal: 'auto' }]}>
       <View>
         <View style={{ marginVertical: gSpace(2) }}>
-          <Button
-            label="Pagos"
-            onPress={() => {
-              navigation.navigate('Payments')
-            }}
-          ></Button>
-        </View>
-        <View style={{ marginVertical: gSpace(2) }}>
-          <Button
-            label="Cortes de caja"
-            onPress={() => {
-              navigation.navigate('Balances')
-            }}
-          ></Button>
+          {canCreateBalance && (
+            <View style={{ marginVertical: gSpace(2) }}>
+              <Button
+                label="Nuevo corte"
+                onPress={() => {
+                  navigation.navigate('BalancesNew')
+                }}
+                icon="add"
+              ></Button>
+            </View>
+          )}
+
+          {canViewBalances && (
+            <View style={{ marginVertical: gSpace(2) }}>
+              <Button
+                label="Cortes de caja"
+                onPress={() => {
+                  navigation.navigate('Balances')
+                }}
+                icon="list"
+              ></Button>
+            </View>
+          )}
+
+          <View style={{ marginVertical: gSpace(2) }}>
+            <Button
+              label="Pagos"
+              onPress={() => {
+                navigation.navigate('Payments')
+              }}
+              icon="money"
+            ></Button>
+          </View>
         </View>
       </View>
     </View>
