a2e452a7251e5879656f5b898b88b27e5311ace7
Raul Zarza <raulzarza.dev@gmail.com>
2024-04-19 15:20:21 -0700

    update (listPayment) add scroll view

--- src/components/ScreenPayments.tsx
+++ src/components/ScreenPayments.tsx
@@ -1,4 +1,4 @@
-import { View } from 'react-native'
+import { ScrollView, View } from 'react-native'
 import React from 'react'
 import PaymentsList from './PaymentsList'
 import { useStore } from '../contexts/storeContext'
@@ -6,13 +6,13 @@ import { useStore } from '../contexts/storeContext'
 export default function ScreenPayments({ navigation }) {
   const { payments } = useStore()
   return (
-    <View>
+    <ScrollView>
       <PaymentsList
         payments={payments}
         onPressRow={(paymentId) => {
           navigation.navigate('PaymentsDetails', { id: paymentId })
         }}
       />
-    </View>
+    </ScrollView>
   )
 }
