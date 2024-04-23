bfde8630bf212340e5ff6c8a8883f8a3cb86348d
Raul Zarza <raulzarza.dev@gmail.com>
2024-04-23 14:16:40 -0700

    update(InputPhone Inputs)  put inputn Phone in componetes guides

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -2,17 +2,32 @@ import { View } from 'react-native'
 import React from 'react'
 import InputCheckbox from './InputCheckbox'
 import ErrorBoundary from './ErrorBoundary'
+import InputPhone from './InputPhone'
 
 const Inputs = () => {
   const [checked, setChecked] = React.useState(false)
+  const [phone, setPhone] = React.useState('')
   return (
     <View>
-      <InputCheckbox label="Checkbox" setValue={setChecked} />
-      <InputCheckbox
-        label="Controlled checked"
-        setValue={setChecked}
-        value={checked}
-      />
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+        <InputCheckbox label="Checkbox" setValue={setChecked} />
+        <InputCheckbox
+          label="Controlled checked"
+          setValue={setChecked}
+          value={checked}
+        />
+      </View>
+      <View style={{ flexDirection: 'column', justifyContent: 'space-around' }}>
+        <InputPhone
+          onChange={(phone) => {
+            setPhone(phone)
+          }}
+        />
+        <InputPhone
+          onChange={(phone) => setPhone(phone)}
+          defaultNumber={'+52554336e345'}
+        />
+      </View>
     </View>
   )
 }
