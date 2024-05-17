f05c8cb1a659d56ffcd8161a1d8f6fcf7e1d8ac4
Raul Zarza <raulzarza.dev@gmail.com>
2024-05-17 16:07:44 -0700

    add labels to markers

--- src/components/ItemsMap.tsx
+++ src/components/ItemsMap.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 import { MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-import Icon from './Icon'
 import ErrorBoundary from './ErrorBoundary'
 import OrderType, { order_status } from '../types/OrderType'
 import theme, { colors } from '../theme'
+import LinkLocation from './LinkLocation'
 const INITIAL_POSITION = [24.145708, -110.311002]
 const LAVARENTA_COORD = [24.150635, -110.316583]
 export type ItemMap = {
@@ -21,10 +21,29 @@ export type ItemsMapProps = {
   //items: ItemMap[]
   orders: OrderType[]
 }
+const SvgIcon = ({ color }) => (
+  <svg
+    width="18"
+    height="18"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="black"
+      strokeWidth="1"
+      fill={color}
+    />
+  </svg>
+)
 const customSvgIcon = (color) =>
+  //@ts-ignore
   new L.DivIcon({
     html: `<svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="12" cy="12" r="10" stroke="black" stroke-width="2" fill=${color} />
+    <circle cx="12" cy="12" r="10" stroke="black" stroke-width="1" fill=${color} />
   </svg>`,
     className: 'custom-svg-icon', // Optional: Add a custom class for additional styling
     iconSize: [14, 14], // size of the icon
@@ -34,18 +53,24 @@ const customSvgIcon = (color) =>
 
 export default function ItemsMap({ orders = [] }: ItemsMapProps) {
   const items = formatItemsMaps(orders)
-  console.log({ items })
   return (
     <View style={styles.container}>
-      <Text>Mapa</Text>
+      <View style={{ flexDirection: 'row' }}>
+        <MarkerInfo label="Vencidas" color={theme.success} />
+        <MarkerInfo label="Reportes" color={theme.error} />
+        <MarkerInfo label="Pedidos" color={theme.warning} />
+        <MarkerInfo label="En renta" color={theme.transparent} />
+      </View>
       <MapContainer
         style={styles.map}
+        //@ts-ignore
         center={INITIAL_POSITION}
         zoom={12}
         scrollWheelZoom={false}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          //@ts-ignore
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
 
@@ -54,12 +79,14 @@ export default function ItemsMap({ orders = [] }: ItemsMapProps) {
             <Marker
               key={item.itemId}
               position={item.coords}
+              //@ts-ignore
               icon={customSvgIcon(item.iconColor)}
             >
               <Popup>
                 <Text>{item.orderFilo}</Text>
                 <br />
                 <Text>{item.clientName}</Text>
+                <LinkLocation location={`${item.coords}`} />
               </Popup>
             </Marker>
           )
@@ -68,6 +95,25 @@ export default function ItemsMap({ orders = [] }: ItemsMapProps) {
     </View>
   )
 }
+export const MarkerInfo = ({
+  label,
+  color
+}: {
+  label: string
+  color: string
+}) => (
+  <View
+    style={{
+      justifyContent: 'center',
+      flexDirection: 'row',
+      marginRight: 8,
+      marginVertical: 16
+    }}
+  >
+    <Text>{label}</Text>
+    <SvgIcon color={color} />
+  </View>
+)
 export const ItemMapE = (props: ItemsMapProps) => {
   return (
     <ErrorBoundary componentName="Map">
@@ -90,6 +136,9 @@ const setLocation = (location: string): [number, number] => {
 }
 const formatItemsMaps = (orders: OrderType[]): ItemMap[] =>
   orders
+    .filter((item) => {
+      return isCoordinates(item.location)
+    })
     ?.map((item) => ({
       itemId: item.id,
       clientName: item.fullName,
@@ -103,19 +152,18 @@ const formatItemsMaps = (orders: OrderType[]): ItemMap[] =>
         return colors.transparent
       })()
     }))
-    .filter((item) => {
-      return isCoordinates(item.coords)
-    })
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    margin: 'auto'
+    margin: 'auto',
+    width: '100%'
   },
   map: {
-    width: 400,
+    width: '100%',
     height: 400
   }
 })
