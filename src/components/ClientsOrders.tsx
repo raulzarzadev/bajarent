bc07af225f69bc634636ad5b5047b59738dcf344
Raul Zarza <raulzarza.dev@gmail.com>
2024-06-23 15:36:42 -0700

    add client hostory title

--- src/components/ClientsOrders.tsx
+++ src/components/ClientsOrders.tsx
@@ -1,23 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text } from 'react-native'
 import React from 'react'
 import { ServiceOrders } from '../firebase/ServiceOrders'
 import { useStore } from '../contexts/storeContext'
 import ListOrders from './ListOrders'
+import { gStyles } from '../styles'
 
 const ClientsOrders = ({ clientId }) => {
   const [orders, setOrders] = React.useState([])
   const { storeId } = useStore()
   React.useEffect(() => {
     ServiceOrders.getClientOrders({ clientId, storeId }).then((res) => {
-      console.log({ res })
       setOrders(res)
     })
   }, [clientId])
 
   return (
-    <View>
+    <ScrollView>
+      <Text style={[gStyles.h3, { marginTop: 16 }]}>Ordenes de cliente</Text>
       <ListOrders orders={orders} />
-    </View>
+    </ScrollView>
   )
 }
 
