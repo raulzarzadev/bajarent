2111bafb63322c3e7cb720854200c50d8aefe8a7
Raul Zarza <raulzarza.dev@gmail.com>
2024-11-21 12:39:35 -0700

    fix (sendMessage) now you can send messages throgth bajarent.app

--- src/libs/whatsapp/sendMessage.ts
+++ src/libs/whatsapp/sendMessage.ts
@@ -1,29 +1,20 @@
-import formatMxWhatsappPhone from './formatMxWhatsapPhone'
 import axios from 'axios'
+
 const sendMessage = async ({ phone, message = 'hola', botId, apiKey }) => {
-  const endpoint = `https://www.builderbot.cloud/api/v2/${botId}/messages`
   if (!phone) return console.log('Phone number is required')
   if (!botId) return console.log('Bot Id is required')
   if (!apiKey) return console.log('Api Key is required')
   if (phone.length < 10) return console.log('Length phone number is invalid')
-  const number = formatMxWhatsappPhone(phone)
 
   const data = {
-    messages: {
-      content: message
-    },
-    number: number
+    message,
+    phone,
+    botId,
+    apiKey
   }
-  axios({
-    method: 'post',
-    url: endpoint,
-    headers: {
-      'Content-Type': 'application/json',
-      'x-api-builderbot': apiKey
-    },
-    data,
-    withCredentials: true
-  })
+
+  axios
+    .post('https://bajarent.app/api/messages', data)
     .then((response) => console.log(response))
     .catch((error) => console.error(error))
 }
