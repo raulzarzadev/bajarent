e1ba116e6b02c20d16d527c867fc9dfbe36d6f25
Raul Zarza <raulzarza.dev@gmail.com>
2025-07-15 17:05:50 -0700

    eliminar el componente TabCashbox y ajustar la lógica de visualización en ScreenStore

--- src/components/ScreenStore5.tsx
+++ src/components/ScreenStore5.tsx
@@ -60,7 +60,6 @@ const ScreenStore = (props) => {
 
   const CheckedTabMovements = CheckedTab(TabMovements)
   const CheckedTabSections = CheckedTab(TabStoreSections)
-  const CheckedTabCashbox = CheckedTab(TabCashbox)
   const CheckedTabStaff = CheckedTab(TabStaff)
   const CheckedTabItems = CheckedTab(TabItems)
   const CheckedTabClients = CheckedTab(TabClients)
@@ -125,23 +124,7 @@ const ScreenStore = (props) => {
               title: '⚙️Ordenes',
               content: <CheckedTabOrders />,
               show: canViewOrders
-            },
-            {
-              title: '*Caja',
-              content: <CheckedTabCashbox />,
-              show: canViewCashbox
-              // icon: 'cashbox'
             }
-            // {
-            //   title: 'Cuentas',
-            //   content: <CheckedStoreCounts />,
-            //   show: false
-            // },
-            // {
-            //   title: 'Mapa',
-            //   content: <CheckedTabMap />,
-            //   show: false
-            // },
           ]}
         />
       )}
@@ -234,120 +217,6 @@ const StoreNumbersRow = () => {
 
 const TabClients = () => {
   return <DisabledView />
-}
-
-const TabCashbox = () => {
-  const { navigate } = useNavigation()
-  const { storeId, sections: storeSections } = useStore()
-  const [progress, setProgress] = useState(0)
-  const [balance, setBalance] = useState<Partial<BalanceType2>>()
-
-  useEffect(() => {
-    ServiceBalances.getLastInDate(storeId, endOfDay(new Date())).then((res) => {
-      setBalance(res[0] || null)
-    })
-  }, [])
-
-  const handleUpdateStoreStatus = async () => {
-    setUpdating(true)
-
-    return await ServiceBalances.createV2(storeId, {
-      progress: (p) => {
-        setProgress(p)
-      },
-      storeSections: storeSections.map((s) => s.id)
-    })
-      .then(async (res) => {
-        setBalance(res)
-        setUpdating(false)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-  const [updating, setUpdating] = useState(false)
-  const endOfDay = (date: Date) => asDate(date.setHours(23, 59, 59, 999))
-
-  const handleGetLastBalanceInDate = (date: Date) => {
-    ServiceBalances.getLastInDate(storeId, endOfDay(date)).then((res) => {
-      setBalance(res[0] || null)
-    })
-  }
-
-  if (balance === undefined) return <Loading />
-  //** disabled functions in tab */
-  const disabled = true
-  return (
-    <ScrollView>
-      <TextInfo
-        defaultVisible
-        type="error"
-        text="Este tab ya no esta actualizado. Pronto desaperecera. La información que hay aquí puede no estar actualizada"
-      />
-      <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-        <Button
-          label="Corte"
-          icon="cashbox"
-          onPress={() => {
-            //@ts-ignore
-            navigate('StackBalances', {
-              screen: 'ScreenBalancesNew'
-            })
-          }}
-          disabled={disabled}
-          variant="ghost"
-        />
-        <Button
-          label="Pagos"
-          icon="money"
-          onPress={() => {
-            //@ts-ignore
-            navigate('StackPayments')
-          }}
-          disabled={disabled}
-          variant="ghost"
-        />
-        <Button
-          label="Retirar"
-          icon="moneyOff"
-          onPress={() => {
-            //@ts-ignore
-            navigate('StackPayments', {
-              screen: 'ScreenRetirementsNew'
-            })
-          }}
-          disabled={disabled}
-          variant="ghost"
-        />
-        <ModalCloseOperations />
-      </View>
-      <HeaderDate
-        debounce={400}
-        label="Cuentas"
-        onChangeDate={(date) => {
-          // setDate(date)
-          handleGetLastBalanceInDate(date)
-        }}
-        documentDate={balance?.createdAt}
-      />
-
-      {/* <DateCounts date={date} /> */}
-      <View style={{ margin: 'auto', marginVertical: 6 }}>
-        <Button
-          progress={progress}
-          disabled={disabled || updating}
-          size="small"
-          fullWidth={false}
-          label="Actualizar"
-          icon="refresh"
-          onPress={async () => {
-            await handleUpdateStoreStatus()
-          }}
-        />
-      </View>
-      <BusinessStatusE balance={balance} />
-    </ScrollView>
-  )
 }
 
 const TabStaff = (props) => {
