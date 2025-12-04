7363838ba08916b76319e943f5ae0ef0e2cdb4d2
Raul Zarza <raulzarza.dev@gmail.com>
2025-12-04 13:04:23 -0700

    feat: implementar la recuperación de cliente en ScreenCustomer usando ServiceCustomers y optimizar la lógica de carga

--- src/components/Customers/ScreenCustomer.tsx
+++ src/components/Customers/ScreenCustomer.tsx
@@ -3,8 +3,10 @@ import ErrorBoundary from '../ErrorBoundary'
 import { useCustomers } from '../../state/features/costumers/costumersSlice'
 import { CustomerOrdersE } from './CustomerOrders'
 import DocMetadata from '../DocMetadata'
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
 import { CustomerCardE } from './CustomerCard'
+import { ServiceCustomers } from '../../firebase/ServiceCustomers'
+import { CustomerType } from '../../state/features/costumers/customerType'
 
 export const CustomerContext = createContext({ customer: null })
 export const useCustomer = () => {
@@ -12,18 +14,23 @@ export const useCustomer = () => {
 }
 const ScreenCustomer = (params) => {
   const customerId = params.route.params.id
-  const { data: customers, loading } = useCustomers()
+  const [customer, setCustomer] = useState<CustomerType | null>()
+  useEffect(() => {
+    if (customerId)
+      ServiceCustomers.get(customerId).then((c) => {
+        setCustomer(c || null)
+      })
+  }, [customerId])
 
-  const customer = customers.find((c) => c.id === customerId)
-  if (loading) return <Text>Cargando...</Text>
-  if (!customer) return <Text>Cliente no encontrado</Text>
+  if (customer === undefined) return <Text>Cargando...</Text>
+  if (customer === null) return <Text>Cliente no encontrado</Text>
 
   return (
     <CustomerContext.Provider value={{ customer }}>
       <ScrollView>
         <DocMetadata item={customer} style={{ margin: 'auto' }} />
         <CustomerCardE customer={customer} />
-        <CustomerOrdersE customerId={customer.id} />
+        <CustomerOrdersE customerId={customer?.id} />
       </ScrollView>
     </CustomerContext.Provider>
   )
