0c4d0b782e763b99d1d7463fefd8a4e94c8ddca9
Raul Zarza <raulzarza.dev@gmail.com>
2025-12-07 12:18:44 -0700

    Refactor components for improved readability and consistency
    - Updated BottomAppBar component to enhance code formatting and maintainability.
    - Refactored ButtonConfirm component for better structure and clarity.
    - Simplified ButtonCreateClient component, improving state management and readability.
    - Enhanced ButtonDeleteItem component with clearer async handling.
    - Streamlined ButtonDownloadCSV component for better organization and readability.
    - Improved CardClient component layout and structure for better maintainability.

--- src/components/ButtonDeleteItem.tsx
+++ src/components/ButtonDeleteItem.tsx
@@ -4,39 +4,39 @@ import { onDeleteItem } from '../firebase/actions/item-actions'
 import ButtonConfirm from './ButtonConfirm'
 
 const ButtonDeleteItem = ({
-  itemId,
-  onDeleted,
-  disabled
+	itemId,
+	onDeleted,
+	disabled
 }: {
-  itemId: string
-  onDeleted?: () => void
-  disabled?: boolean
+	itemId: string
+	onDeleted?: () => void
+	disabled?: boolean
 }) => {
-  const { storeId } = useAuth()
-  const {
-    permissions: { canDeleteItems }
-  } = useEmployee()
-  const handleDelete = async () => {
-    await onDeleteItem({ itemId, storeId })
-      .then(console.log)
-      .catch(console.error)
-      .finally(onDeleted)
-  }
-  return (
-    <ButtonConfirm
-      openDisabled={!canDeleteItems || disabled}
-      text="¿Estas seguro de eliminar este item?"
-      handleConfirm={async () => {
-        return handleDelete()
-      }}
-      openVariant="outline"
-      //justIcon
-      openColor="error"
-      icon="delete"
-      confirmColor="error"
-      confirmLabel="Eliminar"
-    />
-  )
+	const { storeId } = useAuth()
+	const {
+		permissions: { canDeleteItems }
+	} = useEmployee()
+	const handleDelete = async () => {
+		await onDeleteItem({ itemId, storeId })
+			.then(console.log)
+			.catch(console.error)
+			.finally(onDeleted)
+	}
+	return (
+		<ButtonConfirm
+			openDisabled={!canDeleteItems || disabled}
+			text="¿Estas seguro de eliminar este item?"
+			handleConfirm={async () => {
+				return handleDelete()
+			}}
+			openVariant="outline"
+			//justIcon
+			openColor="error"
+			icon="delete"
+			confirmColor="error"
+			confirmLabel="Eliminar"
+		/>
+	)
 }
 
 export default ButtonDeleteItem
