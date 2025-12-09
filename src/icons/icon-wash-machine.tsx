894954b9e1612dc4612d46233773cab3ac087f32
Raul Zarza <raulzarza.dev@gmail.com>
2025-12-08 17:26:10 -0700

    Refactor icon components to use local SVG imports and update fill-rule attributes to camelCase for consistency

--- src/icons/icon-wash-machine.tsx
+++ src/icons/icon-wash-machine.tsx
@@ -10,7 +10,7 @@ export function SmartHomeWashMachine(props) {
       <title>smart-home-wash-machine</title>
       <path
         fill="currentColor"
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M6 4h12a1 1 0 0 1 1 1v3H5V5a1 1 0 0 1 1-1m13 15v-9H5v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1M3 5a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v14a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3zm4 0a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2zm7 2a1 1 0 1 0 0-2a1 1 0 0 0 0 2m4-1a1 1 0 1 1-2 0a1 1 0 0 1 2 0m-4 9a2 2 0 1 1-4 0a2 2 0 0 1 4 0m2 0a4 4 0 1 1-8 0a4 4 0 0 1 8 0"
         clipRule="evenodd"
       />
