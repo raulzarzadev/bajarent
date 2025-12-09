e06a3bc3570ca6a980d438475ddd42e030f86490
Raul Zarza <raulzarza.dev@gmail.com>
2025-12-09 10:40:33 -0700

    Refactor ordersContext and ReduxInitializer for improved readability and consistency

--- src/contexts/ordersContext.tsx
+++ src/contexts/ordersContext.tsx
@@ -1,59 +1,68 @@
-import { where } from 'firebase/firestore'
-import { createContext, type ReactNode, useContext, useEffect, useState } from 'react'
-import { ServiceComments } from '../firebase/ServiceComments'
+import { createContext, type ReactNode, useContext, useState } from 'react'
 import type { ConsolidatedStoreOrdersType } from '../firebase/ServiceConsolidatedOrders'
-import { ServiceOrders } from '../firebase/ServiceOrders'
-import { ServicePayments } from '../firebase/ServicePayments'
-import { formatOrders } from '../libs/orders'
-import { endDate, startDate } from '../libs/utils-date'
 import type { CommentType } from '../types/CommentType'
 import type OrderType from '../types/OrderType'
 import type PaymentType from '../types/PaymentType'
-import { useAuth } from './authContext'
-import { useEmployee } from './employeeContext'
-import { useStore } from './storeContext'
 
-export type FetchTypeOrders = 'all' | 'solved' | 'unsolved' | 'mine' | 'mineSolved' | 'mineUnsolved'
+export type FetchTypeOrders =
+  | 'all'
+  | 'solved'
+  | 'unsolved'
+  | 'mine'
+  | 'mineSolved'
+  | 'mineUnsolved'
 
 export type OrderTypeOption = { label: string; value: FetchTypeOrders }
 export type OrdersContextType = {
-	orders?: Partial<OrderType>[]
-	fetchTypeOrders?: FetchTypeOrders
-	setFetchTypeOrders?: (fetchType: FetchTypeOrders) => void
-	orderTypeOptions?: OrderTypeOption[]
-	handleRefresh?: () => Promise<void> | void
-	reports?: CommentType[]
-	/**
-	 * @deprecated DO NOT USE CONSOLIDATED ORDERS IN NEW CODE
-	 */
-	consolidatedOrders?: ConsolidatedStoreOrdersType
-	repairOrders?: unknown[]
-	payments?: PaymentType[]
-	setOtherConsolidated?: ({ consolidated }: { consolidated: ConsolidatedStoreOrdersType }) => void
+  orders?: Partial<OrderType>[]
+  fetchTypeOrders?: FetchTypeOrders
+  setFetchTypeOrders?: (fetchType: FetchTypeOrders) => void
+  orderTypeOptions?: OrderTypeOption[]
+  handleRefresh?: () => Promise<void> | void
+  reports?: CommentType[]
+  /**
+   * @deprecated DO NOT USE CONSOLIDATED ORDERS IN NEW CODE
+   */
+  consolidatedOrders?: ConsolidatedStoreOrdersType
+  repairOrders?: unknown[]
+  payments?: PaymentType[]
+  setOtherConsolidated?: ({
+    consolidated
+  }: {
+    consolidated: ConsolidatedStoreOrdersType
+  }) => void
 }
 
 let oc = 0
 export const OrdersContext = createContext<OrdersContextType>({})
 
-export const OrdersContextProvider = ({ children }: { children: ReactNode }) => {
-	const [fetchTypeOrders, setFetchTypeOrders] = useState<FetchTypeOrders>(undefined)
+export const OrdersContextProvider = ({
+  children
+}: {
+  children: ReactNode
+}) => {
+  const [fetchTypeOrders, setFetchTypeOrders] =
+    useState<FetchTypeOrders>(undefined)
 
-	oc++
-	if (__DEV__) console.log({ oc })
-	const value = {
-		orders: [],
-		repairOrders: [],
-		setFetchTypeOrders,
-		fetchTypeOrders,
-		orderTypeOptions: [],
-		handleRefresh: () => console.error('handleRefresh no implementado'),
-		reports: [],
-		consolidatedOrders: undefined,
-		payments: [],
-		setOtherConsolidated: () => console.error('setOtherConsolidated no implementado')
-	}
+  oc++
+  if (__DEV__) console.log({ oc })
+  const value = {
+    orders: [],
+    repairOrders: [],
+    setFetchTypeOrders,
+    fetchTypeOrders,
+    orderTypeOptions: [],
+    handleRefresh: () => console.error('handleRefresh no implementado'),
+    reports: [],
+    consolidatedOrders: undefined,
+    payments: [],
+    setOtherConsolidated: () =>
+      console.error('setOtherConsolidated no implementado')
+  }
 
-	return <OrdersContext.Provider value={value}>{children}</OrdersContext.Provider>
+  return (
+    <OrdersContext.Provider value={value}>{children}</OrdersContext.Provider>
+  )
 }
 
 export const useOrdersCtx = () => useContext(OrdersContext)
