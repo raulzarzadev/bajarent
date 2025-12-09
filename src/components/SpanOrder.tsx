8f676a1e1f386d0ade4be9ed7f979b5b2ad3268a
Raul Zarza <raulzarza.dev@gmail.com>
2025-12-09 10:59:34 -0700

    Refactor BusinessStatus, ScreenItemsDetails, and SpanOrder components for improved readability and maintainability
    - Updated import statements for better formatting and organization.
    - Enhanced the structure of the BusinessStatus component, including clearer formatting of the table and row rendering logic.
    - Improved the ScreenItemsDetails component by streamlining the fetchItem logic and enhancing the loading state handling.
    - Refactored the SpanOrder component to utilize Redux for order management, simplifying the order fetching logic and improving performance.
    - Added comments and improved variable naming for better code clarity.

--- src/components/SpanOrder.tsx
+++ src/components/SpanOrder.tsx
@@ -2,7 +2,6 @@ import { isSameDay } from 'date-fns'
 import type { Timestamp } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
 import { Pressable, Text, View } from 'react-native'
-import { useOrdersCtx } from '../contexts/ordersContext'
 import type { ConsolidatedOrderType } from '../firebase/ServiceConsolidatedOrders'
 import { ServiceOrders } from '../firebase/ServiceOrders'
 import useMyNav from '../hooks/useMyNav'
@@ -14,176 +13,179 @@ import { gStyles } from '../styles'
 import theme from '../theme'
 import type OrderType from '../types/OrderType'
 import CurrencyAmount from './CurrencyAmount'
+import { useOrdersRedux } from '../hooks/useOrdersRedux'
 
 const SpanOrder = ({
-	orderId,
-	redirect,
-	showName,
-	showTime,
-	showLastExtension,
-	showDatePaymentsAmount,
-	onRedirect,
-	showItems
+  orderId,
+  redirect,
+  showName,
+  showTime,
+  showLastExtension,
+  showDatePaymentsAmount,
+  onRedirect,
+  showItems
 }: {
-	orderId: string
-	redirect?: boolean
-	showName?: boolean
-	showTime?: boolean
-	showLastExtension?: boolean
-	showDatePaymentsAmount?: Date | Timestamp
-	onRedirect?: () => void
-	showItems?: boolean //<--- show item at the end of line
+  orderId: string
+  redirect?: boolean
+  showName?: boolean
+  showTime?: boolean
+  showLastExtension?: boolean
+  showDatePaymentsAmount?: Date | Timestamp
+  onRedirect?: () => void
+  showItems?: boolean //<--- show item at the end of line
 }) => {
-	const [order, setOrder] = useState<Partial<ConsolidatedOrderType | OrderType>>()
-	const { orders, payments } = useOrdersCtx()
-	const { toOrders } = useMyNav()
+  const [order, setOrder] =
+    useState<Partial<ConsolidatedOrderType | OrderType>>()
+  const { orders } = useOrdersRedux()
+  const { toOrders } = useMyNav()
+  const payments = []
+  //TODO: fetch payments from redux or context
+  useEffect(() => {
+    const searchOrder = async () => {
+      // -> Search and return from CURRENT CONTEXT orders
+      const currentOrdersFound = orders?.find((o) => o.id === orderId)
+      if (currentOrdersFound) {
+        return {
+          ...currentOrdersFound,
+          payments: payments?.filter((p) => p.orderId === orderId)
+        }
+      }
 
-	useEffect(() => {
-		const searchOrder = async () => {
-			// -> Search and return from CURRENT CONTEXT orders
-			const currentOrdersFound = orders?.find(o => o.id === orderId)
-			if (currentOrdersFound) {
-				return {
-					...currentOrdersFound,
-					payments: payments?.filter(p => p.orderId === orderId)
-				}
-			}
+      // -> Search and return from DATABASE orders
+      const dbOrder = await ServiceOrders.get(orderId)
+      if (dbOrder) {
+        return {
+          ...dbOrder,
+          payments: payments?.filter((p) => p.orderId === orderId)
+        }
+      }
+    }
 
-			// -> Search and return from DATABASE orders
-			const dbOrder = await ServiceOrders.get(orderId)
-			if (dbOrder) {
-				return {
-					...dbOrder,
-					payments: payments?.filter(p => p.orderId === orderId)
-				}
-			}
-		}
+    searchOrder().then((res) => {
+      //  console.log({ order: res })
+      setOrder(res)
+    })
+  }, [])
 
-		searchOrder().then(res => {
-			//  console.log({ order: res })
-			setOrder(res)
-		})
-	}, [])
-
-	if (redirect) {
-		return (
-			<Pressable
-				onPress={() => {
-					toOrders({ id: orderId })
-					onRedirect()
-				}}
-			>
-				{order ? (
-					<OrderData
-						order={order}
-						showTime={showTime}
-						showName={showName}
-						showLastExtension={showLastExtension}
-						showDatePaymentsAmount={showDatePaymentsAmount}
-						showItems
-					/>
-				) : (
-					<Text>{orderId}</Text>
-				)}
-			</Pressable>
-		)
-	}
-	return (
-		<>
-			{order ? (
-				<OrderData
-					order={order}
-					showTime={showTime}
-					showName={showName}
-					showLastExtension={showLastExtension}
-					showDatePaymentsAmount={showDatePaymentsAmount}
-					showItems={showItems}
-				/>
-			) : (
-				<Text>{orderId}</Text>
-			)}
-		</>
-	)
+  if (redirect) {
+    return (
+      <Pressable
+        onPress={() => {
+          toOrders({ id: orderId })
+          onRedirect()
+        }}
+      >
+        {order ? (
+          <OrderData
+            order={order}
+            showTime={showTime}
+            showName={showName}
+            showLastExtension={showLastExtension}
+            showDatePaymentsAmount={showDatePaymentsAmount}
+            showItems
+          />
+        ) : (
+          <Text>{orderId}</Text>
+        )}
+      </Pressable>
+    )
+  }
+  return (
+    <>
+      {order ? (
+        <OrderData
+          order={order}
+          showTime={showTime}
+          showName={showName}
+          showLastExtension={showLastExtension}
+          showDatePaymentsAmount={showDatePaymentsAmount}
+          showItems={showItems}
+        />
+      ) : (
+        <Text>{orderId}</Text>
+      )}
+    </>
+  )
 }
 
 const OrderData = ({
-	order,
-	showName,
-	showLastExtension,
-	showDatePaymentsAmount,
-	showItems
+  order,
+  showName,
+  showLastExtension,
+  showDatePaymentsAmount,
+  showItems
 }: {
-	order: Partial<ConsolidatedOrderType | OrderType>
-	showName?: boolean
-	showTime?: boolean
-	showLastExtension?: boolean
-	showDatePaymentsAmount?: Date | Timestamp
-	showItems?: boolean
+  order: Partial<ConsolidatedOrderType | OrderType>
+  showName?: boolean
+  showTime?: boolean
+  showLastExtension?: boolean
+  showDatePaymentsAmount?: Date | Timestamp
+  showItems?: boolean
 }) => {
-	const dateAmounts = payments_amount(
-		order?.payments?.filter(payment =>
-			isSameDay(asDate(payment?.createdAt), asDate(showDatePaymentsAmount))
-		)
-	)
+  const dateAmounts = payments_amount(
+    order?.payments?.filter((payment) =>
+      isSameDay(asDate(payment?.createdAt), asDate(showDatePaymentsAmount))
+    )
+  )
 
-	return (
-		<View style={{ flexDirection: 'row', alignContent: 'center' }}>
-			<Text style={{ marginRight: 4, textAlignVertical: 'center' }}>
-				{order?.folio}-{order?.note}
-			</Text>
-			{showLastExtension && (
-				<Text style={{ fontWeight: 'bold', textAlignVertical: 'center' }}>
-					{translateTime(lastExtensionTime(order), { shortLabel: true })}{' '}
-				</Text>
-			)}
-			{showDatePaymentsAmount && !!dateAmounts?.incomes && (
-				<CurrencyAmount
-					amount={dateAmounts?.incomes}
-					style={{
-						fontWeight: 'bold',
-						...gStyles.helper,
-						backgroundColor: theme.success,
-						borderRadius: 9999,
-						paddingVertical: 0,
-						paddingHorizontal: 3,
-						color: theme.white,
-						textAlignVertical: 'center',
-						marginRight: 4
-					}}
-				/>
-			)}
-			{showName && (
-				<Text
-					style={{
-						//minWidth: 100,
-						flex: 1,
-						textAlignVertical: 'center'
-					}}
-					numberOfLines={1}
-				>
-					{order?.fullName}
-				</Text>
-			)}
-			{showItems && (
-				<Text
-					style={[
-						gStyles.helper,
-						gStyles.tBold,
-						{
-							//textAlignVertical: 'bottom',
-							//borderWidth: 1,
-							// borderColor: 'red',
-							// alignItems: 'stretch'
-						}
-					]}
-				>
-					{order?.items
-						?.map(item => item?.number || item?.serial || item?.id || 'U.U') // this means that any of the info was found
-						.join(', ')}
-				</Text>
-			)}
-		</View>
-	)
+  return (
+    <View style={{ flexDirection: 'row', alignContent: 'center' }}>
+      <Text style={{ marginRight: 4, textAlignVertical: 'center' }}>
+        {order?.folio}-{order?.note}
+      </Text>
+      {showLastExtension && (
+        <Text style={{ fontWeight: 'bold', textAlignVertical: 'center' }}>
+          {translateTime(lastExtensionTime(order), { shortLabel: true })}{' '}
+        </Text>
+      )}
+      {showDatePaymentsAmount && !!dateAmounts?.incomes && (
+        <CurrencyAmount
+          amount={dateAmounts?.incomes}
+          style={{
+            fontWeight: 'bold',
+            ...gStyles.helper,
+            backgroundColor: theme.success,
+            borderRadius: 9999,
+            paddingVertical: 0,
+            paddingHorizontal: 3,
+            color: theme.white,
+            textAlignVertical: 'center',
+            marginRight: 4
+          }}
+        />
+      )}
+      {showName && (
+        <Text
+          style={{
+            //minWidth: 100,
+            flex: 1,
+            textAlignVertical: 'center'
+          }}
+          numberOfLines={1}
+        >
+          {order?.fullName}
+        </Text>
+      )}
+      {showItems && (
+        <Text
+          style={[
+            gStyles.helper,
+            gStyles.tBold,
+            {
+              //textAlignVertical: 'bottom',
+              //borderWidth: 1,
+              // borderColor: 'red',
+              // alignItems: 'stretch'
+            }
+          ]}
+        >
+          {order?.items
+            ?.map((item) => item?.number || item?.serial || item?.id || 'U.U') // this means that any of the info was found
+            .join(', ')}
+        </Text>
+      )}
+    </View>
+  )
 }
 
 export default SpanOrder
