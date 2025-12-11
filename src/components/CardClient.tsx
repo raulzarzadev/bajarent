9e592e64f0cbb6c73a6c75f22b325e7ac6dac13c
Raul Zarza <raulzarza.dev@gmail.com>
2025-12-11 14:24:27 -0700

    Refactor CardClient and ScreenCustomerForm components for improved readability and structure

--- src/components/CardClient.tsx
+++ src/components/CardClient.tsx
@@ -1,5 +1,3 @@
-import { useNavigation } from '@react-navigation/native'
-
 import { Text, View } from 'react-native'
 import { useStore } from '../contexts/storeContext'
 import { ServiceStoreClients } from '../firebase/ServiceStoreClients2'
@@ -10,85 +8,91 @@ import ButtonConfirm from './ButtonConfirm'
 import ErrorBoundary from './ErrorBoundary'
 import ImagePreview from './ImagePreview'
 import SpanMetadata from './SpanMetadata'
+import useMyNav from '../hooks/useMyNav'
 
 export type CardClientProps = {
-	client: Partial<ClientType>
+  client: Partial<ClientType>
 }
 export const CardClient = ({ client }: CardClientProps) => {
-	return (
-		<View style={{ justifyContent: 'center', marginVertical: 6 }}>
-			<SpanMetadata
-				createdAt={client?.createdAt}
-				createdBy={client?.createdBy}
-				id={client?.id}
-				layout="row"
-			/>
-			<View
-				style={{
-					flexDirection: 'row',
-					justifyContent: 'center',
-					alignItems: 'center',
-					marginVertical: 8
-				}}
-			>
-				<ButtonDeleteClient clientId={client?.id} />
-				<Text style={[gStyles.h3, { marginHorizontal: 8 }]}>{client?.name}</Text>
-				<ButtonEditClient clientId={client?.id} />
-			</View>
-			<Text style={[gStyles.tCenter]}>{client?.phone}</Text>
-			<Text style={[gStyles.tCenter]}>{client?.address || ''}</Text>
-			<Text style={[gStyles.tCenter]}>{client?.neighborhood || ''}</Text>
+  return (
+    <View style={{ justifyContent: 'center', marginVertical: 6 }}>
+      <SpanMetadata
+        createdAt={client?.createdAt}
+        createdBy={client?.createdBy}
+        id={client?.id}
+        layout="row"
+      />
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginVertical: 8
+        }}
+      >
+        <ButtonDeleteClient clientId={client?.id} />
+        <Text style={[gStyles.h3, { marginHorizontal: 8 }]}>
+          {client?.name}
+        </Text>
+        <ButtonEditClient clientId={client?.id} />
+      </View>
+      <Text style={[gStyles.tCenter]}>{client?.phone}</Text>
+      <Text style={[gStyles.tCenter]}>{client?.address || ''}</Text>
+      <Text style={[gStyles.tCenter]}>{client?.neighborhood || ''}</Text>
 
-			<View style={{ justifyContent: 'center', flexDirection: 'row' }}>
-				{client?.imageID && <ImagePreview image={client?.imageID} />}
-				{client?.imageHouse && <ImagePreview image={client?.imageHouse} />}
-			</View>
-		</View>
-	)
+      <View style={{ justifyContent: 'center', flexDirection: 'row' }}>
+        {client?.imageID && <ImagePreview image={client?.imageID} />}
+        {client?.imageHouse && <ImagePreview image={client?.imageHouse} />}
+      </View>
+    </View>
+  )
 }
 
 const ButtonDeleteClient = ({ clientId }) => {
-	const { storeId } = useStore()
-	return (
-		<ButtonConfirm
-			icon="delete"
-			openColor="error"
-			openVariant="ghost"
-			openLabel="Eliminar"
-			openSize="xs"
-			justIcon
-			confirmLabel="Eliminar"
-			confirmColor="error"
-			confirmVariant="outline"
-			modalTitle="Eliminar cliente"
-			handleConfirm={async () => {
-				ServiceStoreClients.delete({ itemId: clientId, storeId })
-			}}
-		></ButtonConfirm>
-	)
+  const { storeId } = useStore()
+  return (
+    <ButtonConfirm
+      icon="delete"
+      openColor="error"
+      openVariant="ghost"
+      openLabel="Eliminar"
+      openSize="xs"
+      justIcon
+      confirmLabel="Eliminar"
+      confirmColor="error"
+      confirmVariant="outline"
+      modalTitle="Eliminar cliente"
+      handleConfirm={async () => {
+        ServiceStoreClients.delete({ itemId: clientId, storeId })
+      }}
+    ></ButtonConfirm>
+  )
 }
 
 const ButtonEditClient = ({ clientId }) => {
-	const { navigate } = useNavigation()
-	return (
-		<Button
-			size="xs"
-			icon="edit"
-			label="Editar"
-			justIcon
-			variant="ghost"
-			onPress={() => {
-				//@ts-expect-error
-				navigate('ScreenClientEdit', { id: clientId })
-			}}
-		></Button>
-	)
+  const { toCustomers } = useMyNav()
+
+  return (
+    <Button
+      size="xs"
+      icon="edit"
+      label="Editar"
+      justIcon
+      variant="ghost"
+      onPress={() => {
+        toCustomers({
+          to: 'edit',
+          id: clientId
+        })
+      }}
+    ></Button>
+  )
 }
 
 export const CardClientE = (props: CardClientProps) => (
-	<ErrorBoundary componentName="CardClient">
-		<CardClient {...props} />
-	</ErrorBoundary>
+  <ErrorBoundary componentName="CardClient">
+    <CardClient {...props} />
+  </ErrorBoundary>
 )
 
 export default CardClient
--- src/components/Customers/ScreenCustomerForm.tsx
+++ src/components/Customers/ScreenCustomerForm.tsx
@@ -1,4 +1,5 @@
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import type { RouteProp } from '@react-navigation/native'
 import { useEffect, useState } from 'react'
 import { ScrollView, Text, View } from 'react-native'
 import { useStore } from '../../contexts/storeContext'
@@ -8,50 +9,55 @@ import { gStyles } from '../../styles'
 import ErrorBoundary from '../ErrorBoundary'
 import { FormCustomerE } from './FormCustomer'
 
-const ScreenCustomerForm = navigate => {
-	const customerId = navigate?.route?.params?.id
-	const navigation = useNavigation()
+type ScreenCustomerFormRouteProp = RouteProp<Record<string, { id?: string }>, string>
 
-	const { storeId } = useStore()
-	const { data: customers, create, update } = useCustomers()
+const ScreenCustomerForm = () => {
+  const { params } = useRoute<ScreenCustomerFormRouteProp>()
+  const customerId = params?.id
+  const navigation = useNavigation()
 
-	const defaultCustomer = customers.find(c => c.id === customerId) || null
+  const { storeId } = useStore()
+  const { data: customers, create, update } = useCustomers()
 
-	const handleSubmit = async (customer: NewCustomer) => {
-		if (customerId) {
-			//* Edit  customer
-			await update(customerId, customer)
-			navigation.goBack()
-		} else {
-			//* Create  customer
-			await create(storeId, customer)
-			navigation.goBack()
-		}
-	}
-	const [customer, setCustomer] = useState<NewCustomer>(undefined)
-	useEffect(() => {
-		if (customers && customerId) {
-			const customer = customers.find(c => c.id === customerId)
-			setCustomer(customer)
-		} else {
-			setCustomer(null)
-		}
-	}, [customers])
+  const defaultCustomer = customers.find((c) => c.id === customerId) || null
 
-	if (customerId && customer === undefined) return <Text>Cargando...</Text>
-	//if (customer === null) return <Text>Cliente no encontrado</Text>
+  const handleSubmit = async (customer: NewCustomer) => {
+    if (customerId) {
+      //* Edit  customer
+      await update(customerId, customer)
+      navigation.goBack()
+    } else {
+      //* Create  customer
+      await create(storeId, customer)
+      navigation.goBack()
+    }
+  }
+  const [customer, setCustomer] = useState<NewCustomer>(undefined)
+  useEffect(() => {
+    if (customers && customerId) {
+      const customer = customers.find((c) => c.id === customerId)
+      setCustomer(customer)
+    } else {
+      setCustomer(null)
+    }
+  }, [customers])
 
-	return (
-		<ScrollView>
-			<View style={gStyles.container}>
-				<FormCustomerE onSubmit={handleSubmit} defaultValues={defaultCustomer} />
-			</View>
-		</ScrollView>
-	)
+  if (customerId && customer === undefined) return <Text>Cargando...</Text>
+
+  return (
+    <ScrollView>
+      <View style={gStyles.container}>
+        <FormCustomerE
+          onSubmit={handleSubmit}
+          defaultValues={defaultCustomer}
+        />
+      </View>
+    </ScrollView>
+  )
 }
 export default ScreenCustomerForm
 export const ScreenCustomerFormE = () => (
-	<ErrorBoundary componentName="ScreenCustomerForm">
-		<ScreenCustomerForm />
-	</ErrorBoundary>
+  <ErrorBoundary componentName="ScreenCustomerForm">
+    <ScreenCustomerForm />
+  </ErrorBoundary>
 )
