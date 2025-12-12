dac6b157c82ab5c8c17d3c75ee9b1a28b1bcd01a
Raul Zarza <raulzarza.dev@gmail.com>
2025-12-12 10:29:56 -0700

    Refactor BalanceView and RentsBalance components to conditionally show tabs based on order types; enhance CSV generation for balance reports; improve code readability and structure across StoreBalance components.

--- src/components/StoreBalance/BalanceView.tsx
+++ src/components/StoreBalance/BalanceView.tsx
@@ -14,186 +14,199 @@ import { RepairsBalanceE } from './RepairsBalance'
 import { SalesBalanceE } from './SalesBalance'
 
 export const BalanceView = ({ balance }: BalanceViewProps) => {
-	const { sections } = useStore()
-	const balanceCSV = generateBalanceCSV(balance, {
-		sectionsNames: sections?.map(s => ({
-			sectionId: s.id,
-			sectionName: s.name
-		}))
-	})
+  const { sections } = useStore()
+  const balanceCSV = generateBalanceCSV(balance, {
+    sectionsNames: sections?.map((s) => ({
+      sectionId: s.id,
+      sectionName: s.name
+    }))
+  })
 
-	return (
-		<View>
-			<Text
-				style={{
-					textAlign: 'center',
-					marginBottom: 12,
-					marginTop: 4,
-					...gStyles.helper
-				}}
-			>
-				Última actualización:{' '}
-				<Text style={gStyles.tBold}>
-					{dateFormat(asDate(balance?.createdAt), 'dd/MMM/yy HH:mm')}
-				</Text>
-			</Text>
-			<SpanCopy copyValue={balanceCSV} />
-			<Tabs
-				tabId="store-balance"
-				tabs={[
-					{
-						title: 'General',
-						content: <GeneralBalanceE balance={balance} />,
-						show: true
-						//disabled: true
-					},
-					{
-						title: 'Rentas',
-						content: <RentsBalanceE balance={balance} />,
-						show: true
-					},
-					{
-						title: 'Reparaciones',
-						content: <RepairsBalanceE balance={balance} />,
-						show: true
-						//disabled: true
-					},
-					{
-						title: 'Ventas',
-						content: <SalesBalanceE balance={balance} />,
-						show: true
-						// disabled: true
-					}
-				]}
-			/>
-		</View>
-	)
+  const balanceContainRepairs = balance.orders.some(
+    (order) => order.orderType === order_type.REPAIR
+  )
+  const balanceContainSales = balance.orders.some(
+    (order) => order.orderType === order_type.SALE
+  )
+
+  return (
+    <View>
+      <Text
+        style={{
+          textAlign: 'center',
+          marginBottom: 12,
+          marginTop: 4,
+          ...gStyles.helper
+        }}
+      >
+        Última actualización:{' '}
+        <Text style={gStyles.tBold}>
+          {dateFormat(asDate(balance?.createdAt), 'dd/MMM/yy HH:mm')}
+        </Text>
+      </Text>
+      <SpanCopy copyValue={balanceCSV} />
+      <Tabs
+        tabId="store-balance"
+        tabs={[
+          {
+            title: 'General',
+            content: <GeneralBalanceE balance={balance} />,
+            show: true
+            //disabled: true
+          },
+          {
+            title: 'Rentas',
+            content: <RentsBalanceE balance={balance} />,
+            show: true
+          },
+          {
+            title: 'Reparaciones',
+            content: <RepairsBalanceE balance={balance} />,
+            show: balanceContainRepairs
+            //disabled: true
+          },
+          {
+            title: 'Ventas',
+            content: <SalesBalanceE balance={balance} />,
+            show: balanceContainSales
+            // disabled: true
+          }
+        ]}
+      />
+    </View>
+  )
 }
 export default BalanceView
 export type BalanceViewProps = {
-	balance: StoreBalanceType
+  balance: StoreBalanceType
 }
 export const BalanceViewE = (props: BalanceViewProps) => (
-	<ErrorBoundary componentName="BalanceView">
-		<BalanceView {...props} />
-	</ErrorBoundary>
+  <ErrorBoundary componentName="BalanceView">
+    <BalanceView {...props} />
+  </ErrorBoundary>
 )
 
 //#region generateBalanceCSV
 const generateBalanceCSV = (
-	balance: StoreBalanceType,
-	{
-		sectionsNames
-	}: {
-		sectionsNames: { sectionId: string; sectionName: string }[]
-	}
+  balance: StoreBalanceType,
+  {
+    sectionsNames
+  }: {
+    sectionsNames: { sectionId: string; sectionName: string }[]
+  }
 ) => {
-	// Función auxiliar para formatear valores
-	const formatDate = date => dateFormat(asDate(date), 'dd/MMM/yy HH:mm')
+  // Función auxiliar para formatear valores
+  const formatDate = (date) => dateFormat(asDate(date), 'dd/MMM/yy HH:mm')
 
-	const rentOrders = balance.orders.filter(order => order.orderType === order_type.RENT)
-	const rentPayments = rentOrders.flatMap(o => o.payments)
+  const rentOrders = balance.orders.filter(
+    (order) => order.orderType === order_type.RENT
+  )
+  const rentPayments = rentOrders.flatMap((o) => o.payments)
 
-	const rentedItems = rentOrders.flatMap(o => o.items)
-	const unrentedItems = balance.items
+  const rentedItems = rentOrders.flatMap((o) => o.items)
+  const unrentedItems = balance.items
 
-	const rentedItemsCount = rentedItems.length
-	const unrentedItemsCount = unrentedItems.length
-	const rentedItemsEco = rentedItems
-		.map(i => i?.itemEco || '0')
-		.sort((a, b) => parseInt(b) - parseInt(a))
-		.join(',')
-	const unrentedItemsEco = unrentedItems
-		.map(i => i?.itemEco || '0')
-		.sort((a, b) => parseInt(b) - parseInt(a))
-		.join(',')
+  const rentedItemsCount = rentedItems.length
+  const unrentedItemsCount = unrentedItems.length
+  const rentedItemsEco = rentedItems
+    .map((i) => i?.itemEco || '0')
+    .sort((a, b) => parseInt(b) - parseInt(a))
+    .join(',')
+  const unrentedItemsEco = unrentedItems
+    .map((i) => i?.itemEco || '0')
+    .sort((a, b) => parseInt(b) - parseInt(a))
+    .join(',')
 
-	const {
-		bonus,
-		expense,
-		incomes,
-		canceled,
-		card,
-		cash,
-		total,
-		missing,
-		outcomes,
-		retirements,
-		transfers
-	} = payments_amount(balance.payments)
+  const {
+    bonus,
+    expense,
+    incomes,
+    canceled,
+    card,
+    cash,
+    total,
+    missing,
+    outcomes,
+    retirements,
+    transfers
+  } = payments_amount(balance.payments)
 
-	// Definir tabulación como separador
-	const JUM = `\n'` // SALTO DE LINEA
+  // Definir tabulación como separador
+  const JUM = `\n'` // SALTO DE LINEA
 
-	// Encabezado con separación por tabulaciones
-	let csv = ''
-	csv += `Período ${JUM} ${formatDate(balance.fromDate)} - ${formatDate(balance.toDate)}${JUM}`
-	csv += `Generado el ${JUM} ${formatDate(balance.createdAt)} ${JUM}`
-	//#region  General
-	csv += `RESUMEN GENERAL${JUM}`
+  // Encabezado con separación por tabulaciones
+  let csv = ''
+  csv += `Período ${JUM} ${formatDate(balance.fromDate)} - ${formatDate(
+    balance.toDate
+  )}${JUM}`
+  csv += `Generado el ${JUM} ${formatDate(balance.createdAt)} ${JUM}`
+  //#region  General
+  csv += `RESUMEN GENERAL${JUM}`
 
-	csv += `${JUM}CAJA${JUM}`
-	csv += `Ventas${JUM}${incomes}${JUM}`
-	csv += `Efectivo${JUM}${cash}${JUM}`
-	csv += `Tarjeta${JUM}${card}${JUM}`
-	csv += `Transferencias${JUM}${transfers}${JUM}`
-	csv += `Egresos${JUM}${outcomes}${JUM}`
-	csv += `Retiros${JUM}${retirements}${JUM}`
-	csv += `Bonificaciones${JUM}${bonus}${JUM}`
-	csv += `Gastos${JUM}${expense}${JUM}`
-	csv += `Total${JUM}${total}${JUM}`
-	csv += `Faltante${JUM}${missing}${JUM}`
-	csv += `Cancelados${JUM}${canceled}${JUM}`
+  csv += `${JUM}CAJA${JUM}`
+  csv += `Ventas${JUM}${incomes}${JUM}`
+  csv += `Efectivo${JUM}${cash}${JUM}`
+  csv += `Tarjeta${JUM}${card}${JUM}`
+  csv += `Transferencias${JUM}${transfers}${JUM}`
+  csv += `Egresos${JUM}${outcomes}${JUM}`
+  csv += `Retiros${JUM}${retirements}${JUM}`
+  csv += `Bonificaciones${JUM}${bonus}${JUM}`
+  csv += `Gastos${JUM}${expense}${JUM}`
+  csv += `Total${JUM}${total}${JUM}`
+  csv += `Faltante${JUM}${missing}${JUM}`
+  csv += `Cancelados${JUM}${canceled}${JUM}`
 
-	//#region Rentas
-	const rentAmounts = payments_amount(rentPayments)
-	csv += `${JUM}RESUMEN RENTAS${JUM}`
-	// Pagos de renta
-	csv += `Ventas${JUM}${rentAmounts.incomes}${JUM}`
-	csv += `Efectivo${JUM}${rentAmounts.cash}${JUM}`
-	csv += `Tarjeta${JUM}${rentAmounts.card}${JUM}`
-	csv += `Transferencias${JUM}${rentAmounts.transfers}${JUM}`
-	csv += `Egresos${JUM}${rentAmounts.outcomes}${JUM}`
-	csv += `Retiros${JUM}${rentAmounts.retirements}${JUM}`
-	csv += `Bonificaciones${JUM}${rentAmounts.bonus}${JUM}`
-	csv += `Gastos${JUM}${rentAmounts.expense}${JUM}`
-	csv += `Total${JUM}${rentAmounts.total}${JUM}`
-	csv += `Faltante${JUM}${rentAmounts.missing}${JUM}`
-	csv += `Cancelados${JUM}${rentAmounts.canceled}${JUM}`
-	// Encabezado de pagos de renta por área
-	csv += `${JUM}${JUM}RESUMEN RENTAS POR AREA${JUM}${JUM}`
-	//group by area
-	const rentOrdersByArea = rentOrders.reduce((acc, order) => {
-		const area = order.assignedSection || 'Sin área'
-		acc[area] = acc[area] || []
-		acc[area].push(order)
-		return acc
-	}, {})
-	//#region areas
-	Object.keys(rentOrdersByArea).forEach(area => {
-		const orders = rentOrdersByArea[area]
-		const payments = orders.flatMap(o => o.payments)
-		const amounts = payments_amount(payments)
-		csv += `${JUM}${
-			sectionsNames?.find(s => s.sectionId === area)?.sectionName?.toUpperCase() || ''
-		}${JUM}`
-		csv += `Ventas${JUM}${amounts.incomes}${JUM}`
-		csv += `Efectivo${JUM}${amounts.cash}${JUM}`
-		csv += `Tarjeta${JUM}${amounts.card}${JUM}`
-		csv += `Transferencias
+  //#region Rentas
+  const rentAmounts = payments_amount(rentPayments)
+  csv += `${JUM}RESUMEN RENTAS${JUM}`
+  // Pagos de renta
+  csv += `Ventas${JUM}${rentAmounts.incomes}${JUM}`
+  csv += `Efectivo${JUM}${rentAmounts.cash}${JUM}`
+  csv += `Tarjeta${JUM}${rentAmounts.card}${JUM}`
+  csv += `Transferencias${JUM}${rentAmounts.transfers}${JUM}`
+  csv += `Egresos${JUM}${rentAmounts.outcomes}${JUM}`
+  csv += `Retiros${JUM}${rentAmounts.retirements}${JUM}`
+  csv += `Bonificaciones${JUM}${rentAmounts.bonus}${JUM}`
+  csv += `Gastos${JUM}${rentAmounts.expense}${JUM}`
+  csv += `Total${JUM}${rentAmounts.total}${JUM}`
+  csv += `Faltante${JUM}${rentAmounts.missing}${JUM}`
+  csv += `Cancelados${JUM}${rentAmounts.canceled}${JUM}`
+  // Encabezado de pagos de renta por área
+  csv += `${JUM}${JUM}RESUMEN RENTAS POR AREA${JUM}${JUM}`
+  //group by area
+  const rentOrdersByArea = rentOrders.reduce((acc, order) => {
+    const area = order.assignedSection || 'Sin área'
+    acc[area] = acc[area] || []
+    acc[area].push(order)
+    return acc
+  }, {})
+  //#region areas
+  Object.keys(rentOrdersByArea).forEach((area) => {
+    const orders = rentOrdersByArea[area]
+    const payments = orders.flatMap((o) => o.payments)
+    const amounts = payments_amount(payments)
+    csv += `${JUM}${
+      sectionsNames
+        ?.find((s) => s.sectionId === area)
+        ?.sectionName?.toUpperCase() || ''
+    }${JUM}`
+    csv += `Ventas${JUM}${amounts.incomes}${JUM}`
+    csv += `Efectivo${JUM}${amounts.cash}${JUM}`
+    csv += `Tarjeta${JUM}${amounts.card}${JUM}`
+    csv += `Transferencias
     ${JUM}${amounts.transfers}${JUM}`
-		csv += `Egresos${JUM}${amounts.outcomes}${JUM}`
-		csv += `Retiros${JUM}${amounts.retirements}${JUM}`
-		csv += `Bonificaciones${JUM}${amounts.bonus}${JUM}`
-		csv += `Gastos${JUM}${amounts.expense}${JUM}`
-		csv += `Total${JUM}${amounts.total}${JUM}`
-		csv += `Faltante${JUM}${amounts.missing}${JUM}`
-		csv += `Cancelados${JUM}${amounts.canceled}${JUM}`
-	})
-	//#region items
-	csv += `${JUM}ITEMS${JUM}`
-	csv += `En renta${JUM}${rentedItemsCount}${JUM}${rentedItemsEco}${JUM}`
-	csv += `Disponibles${JUM}${unrentedItemsCount}${JUM}${unrentedItemsEco}${JUM}`
-	csv += `Total${JUM}${rentedItems.length + unrentedItems.length}${JUM}`
-	return csv
+    csv += `Egresos${JUM}${amounts.outcomes}${JUM}`
+    csv += `Retiros${JUM}${amounts.retirements}${JUM}`
+    csv += `Bonificaciones${JUM}${amounts.bonus}${JUM}`
+    csv += `Gastos${JUM}${amounts.expense}${JUM}`
+    csv += `Total${JUM}${amounts.total}${JUM}`
+    csv += `Faltante${JUM}${amounts.missing}${JUM}`
+    csv += `Cancelados${JUM}${amounts.canceled}${JUM}`
+  })
+  //#region items
+  csv += `${JUM}ITEMS${JUM}`
+  csv += `En renta${JUM}${rentedItemsCount}${JUM}${rentedItemsEco}${JUM}`
+  csv += `Disponibles${JUM}${unrentedItemsCount}${JUM}${unrentedItemsEco}${JUM}`
+  csv += `Total${JUM}${rentedItems.length + unrentedItems.length}${JUM}`
+  return csv
 }
--- src/components/StoreBalance/RentsBalance.tsx
+++ src/components/StoreBalance/RentsBalance.tsx
@@ -8,78 +8,81 @@ import BalanceItemsTable from './BalanceItemsTable'
 import { SectionBalanceRentsE } from './SectionBalanceRents'
 
 const RentsBalance = ({ balance }: RentsBalanceProps) => {
-	const { sections: storeSections } = useStore()
-	const rents = balance?.orders?.filter(order => order?.orderType === order_type.RENT)
-	//* group by section
+  const { sections: storeSections } = useStore()
+  const rents = balance?.orders?.filter(
+    (order) => order?.orderType === order_type.RENT
+  )
+  //* group by section
 
-	const groupedBySections = rents?.reduce(
-		(acc, order) => {
-			if (!order?.assignedSection) order.assignedSection = 'withoutSection'
-			if (!acc[order.assignedSection]) acc[order.assignedSection] = []
-			acc[order.assignedSection].push(order)
-			return acc
-		},
-		{} as { [key: string]: StoreBalanceType['orders'] }
-	)
+  const groupedBySections = rents?.reduce((acc, order) => {
+    if (!order?.assignedSection) order.assignedSection = 'withoutSection'
+    if (!acc[order.assignedSection]) acc[order.assignedSection] = []
+    acc[order.assignedSection].push(order)
+    return acc
+  }, {} as { [key: string]: StoreBalanceType['orders'] })
 
-	const sectionsInfo = Object.keys(groupedBySections).map(sectionId => {
-		return {
-			sectionId,
-			sectionName: storeSections?.find(section => section.id === sectionId)?.name || 'Sin asignar'
-		}
-	})
-	const sectionsTabs = sectionsInfo
-		.sort((a, b) => a.sectionName.localeCompare(b.sectionName))
-		.map(({ sectionName, sectionId }) => ({
-			title: sectionName,
-			content: (
-				<SectionBalanceRentsE
-					orders={groupedBySections[sectionId]}
-					balance={balance}
-					title={sectionName}
-					sectionId={sectionId}
-				/>
-			),
-			show: true
-		}))
+  const sectionsInfo = Object.keys(groupedBySections).map((sectionId) => {
+    return {
+      sectionId,
+      sectionName:
+        storeSections?.find((section) => section.id === sectionId)?.name ||
+        'Sin asignar'
+    }
+  })
 
-	return (
-		<View>
-			<Tabs
-				tabId="rents-balance"
-				defaultTab="Todo"
-				tabs={[
-					{
-						title: 'Todo',
-						content: (
-							<SectionBalanceRentsE
-								orders={rents}
-								balance={balance}
-								title={'Todo'}
-								sectionId={'all'}
-							/>
-						),
-						show: true
-					},
-					{
-						title: 'Artículos',
-						content: <BalanceItemsTable balance={balance} />,
-						show: true
-					},
+  const sectionsTabs = sectionsInfo
+    .sort((a, b) => a.sectionName.localeCompare(b.sectionName))
+    .map(({ sectionName, sectionId }) => ({
+      title: sectionName,
+      content: (
+        <SectionBalanceRentsE
+          orders={groupedBySections[sectionId]}
+          balance={balance}
+          title={sectionName}
+          sectionId={sectionId}
+        />
+      ),
+      id: sectionId,
+      show: true
+    }))
 
-					...sectionsTabs
-				]}
-			/>
-		</View>
-	)
+  return (
+    <View>
+      <Tabs
+        tabId="rents-balance"
+        defaultTab="Todo"
+        tabs={[
+          {
+            title: 'Todo',
+            content: (
+              <SectionBalanceRentsE
+                orders={rents}
+                balance={balance}
+                title={'Todo'}
+                sectionId={'all'}
+              />
+            ),
+            show: true
+          },
+          {
+            title: 'Artículos',
+            content: <BalanceItemsTable balance={balance} />,
+            show: true
+          },
+
+          ...sectionsTabs
+        ]}
+      />
+    </View>
+  )
 }
 
 export type RentsBalanceProps = {
-	balance: StoreBalanceType
+  balance: StoreBalanceType
 }
 export const RentsBalanceE = (props: RentsBalanceProps) => (
-	<ErrorBoundary componentName="RentsBalance">
-		<RentsBalance {...props} />
-	</ErrorBoundary>
+  <ErrorBoundary componentName="RentsBalance">
+    <RentsBalance {...props} />
+  </ErrorBoundary>
 )
 export default RentsBalance
