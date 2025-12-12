4e315de79f4312d3ccbdb36b6cc170a2a2434daf
Raul Zarza <raulzarza.dev@gmail.com>
2025-12-12 10:17:44 -0700

    refactor: mejorar la legibilidad y consistencia del código en ScreenStore5

--- src/components/ScreenStore5.tsx
+++ src/components/ScreenStore5.tsx
@@ -20,160 +20,166 @@ import { TabStoreConfiguration } from './TabStoreConfigurationt'
 import TabStoreSections from './TabStoreSections'
 import Tabs from './Tabs'
 
-const ScreenStore = props => {
-	const { user } = useAuth()
-	const { shop } = useShop()
-	const {
-		permissions: { isAdmin, isOwner, store: storePermissions, canManageItems, canEditStaff }
-	} = useEmployee()
+const ScreenStore = (props) => {
+  const { user } = useAuth()
+  const { shop } = useShop()
+  const {
+    permissions: {
+      isAdmin,
+      isOwner,
+      store: storePermissions,
+      canManageItems,
+      canEditStaff
+    }
+  } = useEmployee()
 
-	const scrollViewRef = useRef(null)
+  const scrollViewRef = useRef(null)
 
-	const canViewSections = isAdmin || isOwner || canEditStaff
-	const canViewCashbox = isAdmin || isOwner || storePermissions.canViewCashbox
-	const canViewMovements = isAdmin || isOwner || storePermissions.canViewCashbox
+  const canViewSections = isAdmin || isOwner || canEditStaff
+  const canViewCashbox = isAdmin || isOwner || storePermissions.canViewCashbox
+  const canViewMovements = isAdmin || isOwner || storePermissions.canViewCashbox
 
-	const CheckedTab = <P extends object>(Tab: React.ComponentType<P>) => {
-		const Component = withDisabledCheck(Tab)
-		return Component
-	}
+  const CheckedTab = <P extends object>(Tab: React.ComponentType<P>) => {
+    const Component = withDisabledCheck(Tab)
+    return Component
+  }
 
-	const CheckedTabMovements = CheckedTab(TabMovements)
-	const CheckedTabSections = CheckedTab(TabStoreSections)
-	const CheckedTabStaff = CheckedTab(TabStaff)
-	const CheckedTabItems = CheckedTab(TabItems)
-	const CheckedTabClients = CheckedTab(TabClients)
-	const CheckedTabConfig = CheckedTab(TabStoreConfiguration)
-	const CheckedStoreBalance = CheckedTab(StoreBalanceE)
+  const CheckedTabMovements = CheckedTab(TabMovements)
+  const CheckedTabSections = CheckedTab(TabStoreSections)
+  const CheckedTabStaff = CheckedTab(TabStaff)
+  const CheckedTabItems = CheckedTab(TabItems)
+  const CheckedTabClients = CheckedTab(TabClients)
+  const CheckedTabConfig = CheckedTab(TabStoreConfiguration)
+  const CheckedStoreBalance = CheckedTab(StoreBalanceE)
 
-	if (shop === undefined) return <Loading />
-	if (shop === null) return <Text>Store not found</Text>
-	return (
-		<ScrollView ref={scrollViewRef}>
-			{!!user && (
-				<Tabs
-					tabId="screen-store"
-					tabs={[
-						{
-							title: 'Información',
-							content: <StoreDetailsE {...props} />,
-							show: true,
-							icon: 'info'
-						},
-						{
-							title: 'Artículos',
-							content: <CheckedTabItems />,
-							show: canManageItems,
-							icon: 'camera'
-						},
+  if (shop === undefined) return <Loading />
+  if (shop === null) return <Text>Store not found</Text>
+  return (
+    <ScrollView ref={scrollViewRef}>
+      {!!user && (
+        <Tabs
+          tabId="screen-store"
+          tabs={[
+            {
+              title: 'Información',
+              content: <StoreDetailsE {...props} />,
+              show: true,
+              icon: 'info'
+            },
+            {
+              title: 'Artículos',
+              content: <CheckedTabItems />,
+              show: canManageItems,
+              icon: 'camera'
+            },
 
-						{
-							title: 'Balance',
-							content: <CheckedStoreBalance />,
-							show: canViewCashbox,
-							icon: 'balance'
-							// icon: 'balance'
-						},
+            {
+              title: 'Balance',
+              content: <CheckedStoreBalance />,
+              show: canViewCashbox,
+              icon: 'balance'
+              // icon: 'balance'
+            },
 
-						{
-							title: 'Staff',
-							content: <CheckedTabStaff {...props} />,
-							show: canViewSections,
-							icon: 'profile'
-							//icon: 'profile'
-						},
-						{
-							title: 'Areas',
-							content: <CheckedTabSections />,
-							show: canViewSections,
-							icon: 'list'
-							// icon: 'windows'
-						},
-						{
-							title: 'Clientes',
-							content: <CheckedTabClients />,
-							show: false,
-							icon: 'customerCard'
-						},
-						{
-							title: ' Historal',
-							content: <CheckedTabMovements />,
-							show: canViewMovements,
-							icon: 'history'
-						},
-						{
-							title: 'Chatbot',
-							content: <ScreenChatbotE />,
-							show: isAdmin,
-							icon: 'chatbot'
-						},
-						{
-							title: 'Configuración',
-							content: <CheckedTabConfig />,
-							show: isAdmin,
-							icon: 'settings'
-						}
-					]}
-				/>
-			)}
-		</ScrollView>
-	)
+            {
+              title: 'Staff',
+              content: <CheckedTabStaff {...props} />,
+              show: canViewSections,
+              icon: 'profile'
+              //icon: 'profile'
+            },
+            {
+              title: 'Areas',
+              content: <CheckedTabSections />,
+              show: canViewSections,
+              icon: 'list'
+              // icon: 'windows'
+            },
+            {
+              title: 'Clientes',
+              content: <CheckedTabClients />,
+              show: false,
+              icon: 'customerCard'
+            },
+            {
+              title: ' Historal',
+              content: <CheckedTabMovements />,
+              show: canViewMovements,
+              icon: 'history'
+            },
+            {
+              title: 'Chatbot',
+              content: <ScreenChatbotE />,
+              show: isAdmin,
+              icon: 'chatbot'
+            },
+            {
+              title: 'Configuración',
+              content: <CheckedTabConfig />,
+              show: isAdmin,
+              icon: 'settings'
+            }
+          ]}
+        />
+      )}
+    </ScrollView>
+  )
 }
 
 const TabMovements = () => {
-	return (
-		<View>
-			<Tabs
-				tabs={[
-					{
-						title: 'Ordenes',
-						content: <BalanceOrders />,
-						show: true
-					},
-					{
-						title: 'Movimientos',
-						content: <ListMovements />,
-						show: true
-					},
-					{
-						title: 'Trabajo actual',
-						content: <ListMovements />,
-						show: true
-					}
-				]}
-			></Tabs>
-			{/* <CurrentWorkListE />
-      <ListMovements /> */}
-		</View>
-	)
+  return (
+    <View>
+      <Tabs
+        tabs={[
+          {
+            title: 'Ordenes',
+            content: <BalanceOrders />,
+            show: true
+          },
+          {
+            title: 'Movimientos',
+            content: <ListMovements />,
+            show: true
+          },
+          {
+            title: 'Trabajo actual',
+            content: <CurrentWorkListE />,
+            show: true
+          }
+        ]}
+      ></Tabs>
+    </View>
+  )
 }
 
 const TabClients = () => {
-	return <DisabledView />
+  return <DisabledView />
 }
 
-const TabStaff = props => {
-	return (
-		<View>
-			<ScreenStaffE {...props} />
-		</View>
-	)
+const TabStaff = (props) => {
+  return (
+    <View>
+      <ScreenStaffE {...props} />
+    </View>
+  )
 }
 
 const TabItems = () => {
-	return (
-		<View style={[gStyles.container, { marginBottom: gSpace(16), maxWidth: 1200 }]}>
-			<ScreenItems />
-		</View>
-	)
+  return (
+    <View
+      style={[gStyles.container, { marginBottom: gSpace(16), maxWidth: 1200 }]}
+    >
+      <ScreenItems />
+    </View>
+  )
 }
 
 export default ScreenStore
 
-export const ScreenStoreE = props => {
-	return (
-		<ErrorBoundary componentName="ScreenStore5">
-			<ScreenStore {...props} />
-		</ErrorBoundary>
-	)
+export const ScreenStoreE = (props) => {
+  return (
+    <ErrorBoundary componentName="ScreenStore5">
+      <ScreenStore {...props} />
+    </ErrorBoundary>
+  )
 }
