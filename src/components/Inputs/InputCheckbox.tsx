25ba0608df0d3580c741563cb933b5cb147c193f
Raul Zarza <raulzarza.dev@gmail.com>
2025-12-14 17:47:45 -0700

    refactor: mejorar la legibilidad y consistencia del código en varios componentes; ajustar la estructura y formato

--- src/components/Inputs/InputCheckbox.tsx
+++ src/components/Inputs/InputCheckbox.tsx
@@ -1,83 +1,105 @@
-import { Pressable, Text, type TextStyle, View, type ViewStyle } from 'react-native'
+import {
+  Pressable,
+  Text,
+  type TextStyle,
+  View,
+  type ViewStyle
+} from 'react-native'
 import Icon2 from '../../icons'
 import theme from '../../theme'
 import Icon, { type IconName } from '../Icon'
 export type InputCheckboxProps = {
-	label?: string
-	setValue: (value: boolean) => void
-	value?: boolean
-	style?: ViewStyle
-	textStyle?: TextStyle
-	color?: string
-	disabled?: boolean
-	iconCheck?: IconName
-	iconLabel?: IconName
-	variant?: 'ghost' | 'outline'
+  label?: string
+  setValue: (value: boolean) => void
+  value?: boolean
+  style?: ViewStyle
+  textStyle?: TextStyle
+  color?: string
+  disabled?: boolean
+  iconCheck?: IconName
+  iconLabel?: IconName
+  variant?: 'ghost' | 'outline'
 }
 const InputCheckbox = ({
-	label,
-	setValue,
-	value = false,
-	style,
-	textStyle,
-	color = theme.success,
-	disabled,
-	iconLabel,
-	iconCheck,
-	variant
+  label,
+  setValue,
+  value = false,
+  style,
+  textStyle,
+  color = theme.success,
+  disabled,
+  iconLabel,
+  iconCheck,
+  variant
 }: InputCheckboxProps) => {
-	const capitalizedLabel = label?.charAt(0)?.toUpperCase() + label?.slice(1) || ''
+  const capitalizedLabel =
+    label?.charAt(0)?.toUpperCase() + label?.slice(1) || ''
 
-	const handlePress = (newState: boolean) => {
-		setValue(newState) // Actualizar directamente el estado padre
-	}
-	return (
-		<Pressable
-			disabled={disabled}
-			onPress={() => handlePress(!value)}
-			style={[
-				{
-					flexDirection: 'row',
-					alignItems: 'center',
-					padding: 2
-				},
-				style
-			]}
-		>
-			<View
-				style={{
-					opacity: disabled ? 0.4 : 1,
-					backgroundColor:
-						variant === 'ghost' && !value ? 'transparent' : value ? color : theme.white,
-					borderWidth: 1,
-					borderColor: variant === 'ghost' ? 'transparent' : value ? theme.white : color,
-					borderRadius: 99,
-					padding: 2,
-					marginRight: 2,
-					width: 20,
-					height: 20,
-					justifyContent: 'center',
-					alignItems: 'center'
-				}}
-			>
-				<Icon2 name={iconCheck || 'done'} color={value ? theme.white : color} size={16} />
-			</View>
-			{iconLabel && <Icon icon={iconLabel} color={color} size={16} />}
-			<Text
-				style={[
-					{
-						textDecorationLine: 'none',
-						marginLeft: 2,
-						opacity: disabled ? 0.4 : 1
-						//  textTransform: 'capitalize'
-					},
-					textStyle
-				]}
-			>
-				{capitalizedLabel}
-			</Text>
-		</Pressable>
-	)
+  const handlePress = (newState: boolean) => {
+    setValue(newState) // Actualizar directamente el estado padre
+  }
+  return (
+    <Pressable
+      disabled={disabled}
+      onPress={() => handlePress(!value)}
+      style={[
+        {
+          flexDirection: 'row',
+          alignItems: 'center',
+          padding: 2
+        },
+        style
+      ]}
+    >
+      <View
+        style={{
+          opacity: disabled ? 0.4 : 1,
+          backgroundColor: 'transparent',
+          borderWidth: 2,
+          borderColor: color,
+          borderRadius: 99,
+          padding: 4,
+          marginRight: 2,
+          width: 24,
+          height: 24,
+          justifyContent: 'center',
+          alignItems: 'center',
+          flex: 1
+        }}
+      >
+        {iconCheck ? (
+          <Icon2
+            name={iconCheck}
+            color={value ? color : 'transparent'}
+            size={16}
+          />
+        ) : (
+          <View
+            style={{
+              width: 14,
+              height: 14,
+              borderRadius: 999,
+              backgroundColor: value ? color : 'transparent'
+            }}
+          />
+        )}
+      </View>
+      {iconLabel && <Icon icon={iconLabel} color={color} size={16} />}
+      <Text
+        style={[
+          {
+            textDecorationLine: 'none',
+            marginLeft: 2,
+            opacity: disabled ? 0.4 : 1
+            //  textTransform: 'capitalize'
+          },
+          textStyle
+        ]}
+      >
+        {capitalizedLabel}
+      </Text>
+    </Pressable>
+  )
 }
 
 export default InputCheckbox
